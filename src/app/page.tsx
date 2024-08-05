a909b99e8d2c0fc8b94702b22b70c190df63a828
rodrigoofig <rodres2002@gmail.com>
2024-08-05 16:38:15 +0100

    checking if we already logged in working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
 
-import { useSession } from "next-auth/react";
-import { LoginForm } from "./login/form";
 
-export default function Home() {
-  const [session, loading] = useSession();
-  if(!session){
-    return (
-      <main>
-        <LoginForm />
-      </main>
-    )
+import { LoginForm } from "./login/form";
+import { cookies } from 'next/headers';
+
+
+
+const getToken = async () => {
+  const token = cookies().get('next-auth.session-token')
+  console.log('SIIIU', token);
+  return token;
+};
+
+export default async function Home() {
+
+  const token = await getToken();
+  console.log(token);
+
+  if (token) {
+    return <h1>Welcome to Dashboard</h1>;
   }
+  return (
+    <>
+      <LoginForm />;
+    </>
+  );
+  
 }
