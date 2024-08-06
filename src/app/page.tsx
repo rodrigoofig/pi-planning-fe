8e5344014fe0aa8027bf68c88ef3876a907a4f2f
johnnytony <joaopro23@gmail.com>
2024-08-06 14:41:30 +0100

    Changed to session provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,8 @@
+"use client";
 
+import Home from "@/components/homepage";
+import {SessionProvider } from "next-auth/react"
 
-import { LoginForm } from "./login/form";
-import { cookies } from 'next/headers';
-
-
-
-const getToken = async () => {
-  const token = cookies().get('next-auth.session-token')
-  console.log('SIIIU', token);
-  return token;
-};
-
-export default async function Home() {
-
-  const token = await getToken();
-  console.log(token);
-
-  if (token) {
-    return <h1>Welcome to Dashboard</h1>;
-  }
-  return (
-    <>
-      <LoginForm />;
-    </>
-  );
-  
+export default function Page() {
+  return <SessionProvider><Home/></SessionProvider>
 }
