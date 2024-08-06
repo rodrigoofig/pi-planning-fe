d06f7c03085ebe9d023a966562e8f85350b324db
rodrigoofig <rodres2002@gmail.com>
2024-08-06 15:05:28 +0100

    more pages

---
+++ src/app/chat/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Chat({params}: {params: any}) {
+
+  return <main>
+    <h1>Chat: {params.slug}</h1>
+  </main>
+    
+}
---
+++ src/app/component/chatReference.tsx
@@ -0,0 +1,6 @@
+export default function ChatReference({props}: {props: any}) {
+    return <main>
+      <h1>ChatReference: {props.id}</h1>
+    </main>
+
+}
